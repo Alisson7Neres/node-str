89eb93680c4e5f59915b0ff1ed5935bc4d940858
Alisson7Neres <alisson.neres@hotmail.com>
2021-09-07 08:34:42 -0300

    Implementando a validação

--- src/controlles/product-controller.js
+++ src/controlles/product-controller.js
@@ -1,6 +1,7 @@
 'use strict';
 const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
+const ValidationContract = require('../validators/fluent-validator')
 
 exports.get = (req, res, next) => {
     Product.find({ active: true }, 'title price slug').then(data => {
@@ -33,8 +34,20 @@ exports.getById = (req, res, next) => {
 
 
 exports.post = (req, res, next) => {
+    let contract = new ValidationContract();
+    contract.hasMinLen(req.body.title, 3, 'O título deve conter pelo menos 3 caracteres')
+    contract.hasMinLen(req.body.description, 3, 'A descrição deve conter pelo menos 3 caracteres')
+    contract.hasMinLen(req.body.slug, 3, 'O slug deve conter pelo menos 3 caracteres')
+
+    // Se os dados forem inválidos
+    if(!contract.isValid()){
+        res.status(400).send(contract.errors()).end();
+        return;
+    }
+
     var product = new Product(req.body);
-    product.save().then(x => {
+    product.save()
+    .then(x => {
         res.status(201).send({ message: 'Produto cadastrado com sucesso' });
     }).catch(e => {
         res.status(400).send({ message: 'Falha ao cadastrar produto', data: e });
